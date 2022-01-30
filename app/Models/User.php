8662b41f0f308e03af9e12af3f8b5ab8e9d72f02
drgavali9 <drgavali9@gmail.com>
2022-01-31 00:02:12 +0530

    update setup telwinds dashboard page

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,9 +23,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'date_of_birth',
+        'status'
     ];
 
     /**
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-full bg-white" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,11 +17,11 @@
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased h-full">
         @inertia
 
         @env ('local')
-            <script src="http://localhost:8080/js/bundle.js"></script>
+            {{-- <script src="http://localhost:8080/js/bundle.js"></script> --}}
         @endenv
     </body>
 </html>
