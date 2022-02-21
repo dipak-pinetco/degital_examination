516be83da73bea48f3f7b8b0fcd16d5f8429594d
dipakpinetco <dipak@pinetco.com>
2022-02-21 11:41:55 +0530

    update code

--- tests/cypress/integration/Common/auth.js
+++ tests/cypress/integration/Common/auth.js
@@ -2,8 +2,8 @@
 
 context('Actions', () => {
     before(() => {
-        cy.refreshDatabase();
-        cy.seed('DatabaseSeeder');
+        cy.refreshDatabase()
+            .seed('DatabaseSeeder');
     });
 
     it('go to home page', () => {
@@ -12,20 +12,22 @@ context('Actions', () => {
 
     // Validation Flow
     it('login validation check', () => {
-        cy.visit({ route: 'index' });
-        cy.get('.justify-end > .text-gray-500').contains('Sign in').click()
+        cy.visit({ route: 'index' })
+            .get('.justify-end > .text-gray-500')
+            .contains('Sign in')
+            .click()
 
         // Genreate All Error
         cy.get('.flex > .inline-flex').click();
-        cy.get('.text-red-500').contains('The role field is required.');
-        cy.get('.text-red-600').contains('The email field is required.');
-        cy.get(':nth-child(4) > .text-red-600').contains('The password field is required.');
+        cy.get('.text-red-500').contains('The role field is required.')
+            .get('.text-red-600').contains('The email field is required.')
+            .get(':nth-child(4) > .text-red-600').contains('The password field is required.');
 
-        cy.get('#roles').select('admin');
-        cy.get('#email').type('drgavali9@gmail.com');
-        cy.get('#password').type('Admin@123');
-        cy.get('#remember_me').check();
-        cy.get('.flex > .inline-flex').click();
+        cy.get('#roles').select('admin')
+            .get('#email').type('drgavali9@gmail.com')
+            .get('#password').type('Admin@123')
+            .get('#remember_me').check()
+            .get('.flex > .inline-flex').click();
 
         cy.get('.text-red-600').contains('These credentials do not match our records.');
         cy.get('#password').type('password');
@@ -37,11 +39,11 @@ context('Actions', () => {
         cy.get('.underline').contains('Forgot your password?').click()
 
         // Genreate All Error
-        cy.get('.inline-flex').click();
-        cy.get('.text-red-600').contains('The email field is required.');
-        cy.get('#email').type('drgavali9@gmail.com');
-        cy.get('.inline-flex').click();
-        cy.get('.text-red-600').contains("We can't find a user with that email address.");
+        cy.get('.inline-flex').click()
+            .get('.text-red-600').contains('The email field is required.')
+            .get('#email').type('drgavali9@gmail.com')
+            .get('.inline-flex').click()
+            .get('.text-red-600').contains("We can't find a user with that email address.");
 
     });
 
@@ -50,58 +52,58 @@ context('Actions', () => {
         cy.get('.ml-8').contains('Sign up').click()
 
         // Genreate All Error
-        cy.get('.inline-flex').click();
-        cy.get(':nth-child(2) > .mt-4 > .text-red-500').contains('The school id field is required.')
-        cy.get(':nth-child(3) > .mt-4 > .text-red-500').contains('The role field is required.');
-        cy.get(':nth-child(4) > .text-red-600').contains('The first name field is required.');
-        cy.get(':nth-child(5) > .text-red-600').contains('The last name field is required.');
-        cy.get(':nth-child(6) > .text-red-600').contains('The email field is required.');
-        cy.get(':nth-child(7) > .text-red-600').contains('The mobile field is required.');
-        cy.get(':nth-child(8) > .text-red-600').contains('The date of birth field is required.');
-        cy.get(':nth-child(9) > .mt-4 > .text-red-500').contains('The gender field is required.');
-        cy.get(':nth-child(10) > .text-red-600').contains('The password field is required.');
+        cy.get('.inline-flex').click()
+            .get(':nth-child(2) > .mt-4 > .text-red-500').contains('The school id field is required.')
+            .get(':nth-child(3) > .mt-4 > .text-red-500').contains('The role field is required.')
+            .get(':nth-child(4) > .text-red-600').contains('The first name field is required.')
+            .get(':nth-child(5) > .text-red-600').contains('The last name field is required.')
+            .get(':nth-child(6) > .text-red-600').contains('The email field is required.')
+            .get(':nth-child(7) > .text-red-600').contains('The mobile field is required.')
+            .get(':nth-child(8) > .text-red-600').contains('The date of birth field is required.')
+            .get(':nth-child(9) > .mt-4 > .text-red-500').contains('The gender field is required.')
+            .get(':nth-child(10) > .text-red-600').contains('The password field is required.');
     });
 
     it('already registered flow check', () => {
-        cy.visit({ route: 'index' });
-        cy.get('.ml-8').contains('Sign up').click()
-        cy.get('.underline').contains('Already registered?').click()
-        cy.intercept('/login');
+        cy.visit({ route: 'index' })
+            .get('.ml-8').contains('Sign up').click()
+            .get('.underline').contains('Already registered?').click()
+            .intercept('/login');
     });
 
     it('admin register', () => {
         cy.visit({ route: 'index' });
         cy.get('.ml-8').contains('Sign up').click()
 
-        cy.get('#school').select('1');
-        cy.get('#roles').select('admin');
-        cy.get('#first_name').type('Dipak');
-        cy.get('#last_name').type('Gavali');
-        cy.get('#email').type('drgavali9@gmail.com');
-        cy.get('#mobile').type('+91 9173921432');
-        cy.get('#date_of_birth').type('1995-09-10');
-        cy.get('#gender').select('Male');
-        cy.get('#password').type('password');
-        cy.get('#password_confirmation').type('password');
-        cy.get('.inline-flex').click();
+        cy.get('#school').select('1')
+            .get('#roles').select('admin')
+            .get('#first_name').type('Dipak')
+            .get('#last_name').type('Gavali')
+            .get('#email').type('drgavali9@gmail.com')
+            .get('#mobile').type('+91 9173921432')
+            .get('#date_of_birth').type('1995-09-10')
+            .get('#gender').select('Male')
+            .get('#password').type('password')
+            .get('#password_confirmation').type('password')
+            .get('.inline-flex').click();
 
-        cy.intercept('/dashboard');
-        cy.get('form > .text-white').click();
-        cy.intercept('/');
+        cy.intercept('/dashboard')
+            .get('form > .text-white').click()
+            .intercept('/');
     });
 
     it('admin user can login', () => {
         cy.visit({ route: 'index' });
         cy.get('.justify-end > .text-gray-500').contains('Sign in').click()
 
-        cy.get('#roles').select('admin');
-        cy.get('#email').type('drgavali9@gmail.com');
-        cy.get('#password').type('password');
-        cy.get('#remember_me').check();
-        cy.get('.flex > .inline-flex').click();
+        cy.get('#roles').select('admin')
+            .get('#email').type('drgavali9@gmail.com')
+            .get('#password').type('password')
+            .get('#remember_me').check()
+            .get('.flex > .inline-flex').click();
 
-        cy.intercept('/dashboard');
-        cy.get('form > .text-white').click();
-        cy.intercept('/');
+        cy.intercept('/dashboard')
+            .get('form > .text-white').click()
+            .intercept('/');
     });
 })
